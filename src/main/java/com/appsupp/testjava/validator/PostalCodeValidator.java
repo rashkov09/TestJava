10354d214460fe25247f11649f20ad37c6ac1e6c
Rashkov <drashkov@flatex.com>
2024-12-03 16:18:58 +0200

    Change in style

--- src/main/java/com/appsupp/testjava/validator/PostalCodeValidator.java
+++ src/main/java/com/appsupp/testjava/validator/PostalCodeValidator.java
@@ -3,6 +3,15 @@ package com.appsupp.testjava.validator;
 import java.util.regex.Pattern;
 
 public class PostalCodeValidator implements Validator {
+    /**
+     * The regular expression pattern used to validate postal codes.
+     * The pattern consists of the following elements:
+     *   ^     asserts the start of the line
+     *   [0-9] matches any digit (0-9)
+     *   {5}   specifies that the preceding element should be matched exactly 5 times
+     *   $     asserts the end of the line
+     * In summary, this pattern matches any string that consists of exactly 5 digits and has no spaces.
+     */
     private static  final String POSTAL_CODE_REGEX = "^[0-9]{5}$";
     @Override
     public boolean isValid(String postalCode) {
